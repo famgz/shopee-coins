a6beb8c5eead9e37cbe02c3c00390407fb995722
famgz <famgz@proton.me>
2025-08-14 20:00:07 -0300

    feat: logging improvements

--- main.py
+++ main.py
@@ -24,12 +24,13 @@ def main():
     transactions = get_transactions()
     previous_transactions_qty = len(transactions.keys())
     COOKIE_KEY = "SPC_EC"
-    _cookies = Cookies(cookies_dir)
-    _cookies.check_expired_cookies()
-    cookies = _cookies.get_cookies()
+    cookies = Cookies(cookies_dir)
+    cookies.check_expired_cookies()
+    cookies = cookies.get_cookies()
     cookies = {COOKIE_KEY: cookies[COOKIE_KEY]}
     per_type = {}
     total = {"+": 0, "-": 0}
+    new = {"+": 0, "-": 0}
     infos = {"name": [], "content": []}
     LIMIT = 1000
     items_count = 0
@@ -48,12 +49,12 @@ def main():
         )
         data = response.json()
         items = data["items"]
-        print(f"{len(items)} items")
+        print(f"{len(items)} items{' '*50}", end='\r')
         if not items:
             break
         items_count += len(items)
-        parsed_items = {str(item["id"]): item for item in items}
-        transactions.update(parsed_items)
+        parsed_new_items = {str(item["id"]): item for item in items if not transactions.get(str(item["id"]))}
+        transactions.update(parsed_new_items)
         for item in items:
             reason = item["info"]["reason"]
             name = item["name"].strip()
@@ -66,21 +67,28 @@ def main():
             key = reason or content or name
             key = key.strip()
             per_type.setdefault(key, {"+": 0, "-": 0})
+            is_new_item = parsed_new_items.get(str(item["id"]))
             if amount > 0:
                 per_type[key]["+"] += amount
                 total["+"] += amount
+                if is_new_item:
+                    new["+"] += amount
             else:
                 per_type[key]["-"] += amount
                 total["-"] += amount
+                if is_new_item:
+                    new["-"] += amount
         offset += LIMIT
         sleep(response.elapsed.total_seconds())
 
     new_transactions_qty = len(transactions.keys()) - previous_transactions_qty
+    print()
     print(f"{items_count} transactions found")
-    print(f"{new_transactions_qty} new transactions saved")
+    print(f"{new_transactions_qty} new transactions saved {new}")
     print(f"\n{total['+']} coins earned")
     print(f"{total['-']} coins redeemed")
     print(f"{total['+'] - abs(total['-'])} coins available")
+    print()
 
     per_type = dict(sorted(per_type.items(), key=lambda x: x[1]["+"], reverse=True))
     summary = {"total": total, "per_type": per_type}
